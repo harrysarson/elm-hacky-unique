290f7c96f7177e03fba7943db5987810fd1787f0
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-10-10 16:03:19 +0100

    Create package.

--- src/Unique.elm
+++ src/Unique.elm
@@ -1,10 +1,25 @@
 module Unique exposing (Unique, unique)
 
+{-| Create unique types in elm.
 
+@docs Unique, unique
+
+-}
+
+
+{-| The unique type.
+Any record or custom type containing `Unique` values will only compare as equal
+if they are exactly the same object.
+
+See the README for an example.
+
+-}
 type Unique
     = UniqueImpl Float
 
 
+{-| Generate unique types.
+-}
 unique : () -> Unique
 unique _ =
-    UniqueImpl (0/0)
+    UniqueImpl (0 / 0)
